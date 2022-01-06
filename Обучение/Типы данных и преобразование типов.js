b83668df76e3ecac933cf09d9be40e6f13461d69
ftn221 <ftn221@gmail.com>
2022-01-06 10:49:01 +0300

    Добавлен урок "Основные операторы, операторы сравнения и логические значения"

--- Обучение/Типы данных и преобразование типов.js
+++ Обучение/Типы данных и преобразование типов.js
@@ -1,3 +1,4 @@
+/*
 Джаваскрипт - динамически типизированный язык, это значит что тип 
 данных переменной может изменяться
 
@@ -86,3 +87,4 @@ let name = "Ilya";
 alert( `hello ${1}` ); // hello 1
 alert( `hello ${"name"}` ); // hello name
 alert( `hello ${name}` ); // hello Ilya
+*/
